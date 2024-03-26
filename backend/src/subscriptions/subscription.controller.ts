b1fa2d4cb82ee96af3a95ad8bb7177c2453f3669
Wendel Moro <wendelcmoro@gmail.com>
2024-03-26 20:32:05 -0300

    adicionando rotas backend agrupoando dados por mês com parâmetros opcionais

--- backend/src/subscriptions/subscription.controller.ts
+++ backend/src/subscriptions/subscription.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { Subscription } from './subscription.entity';
 import { SubscriptionService } from './subscription.service';
 
@@ -9,5 +9,69 @@ export class SubscriptionController {
   @Get()
   async findAll(): Promise<Subscription[]> {
     return await this.subscriptionService.findAll();
+  }
+
+  @Get('/subscriptions-per-month')
+  async getSubscriptionsPerMonth(
+    @Query('year')
+    year?: number,
+    @Query('status')
+    status?: number,
+    @Query('date_filter')
+    date_filter?: number,
+  ): Promise<{ month: number; count: number }[]> {
+    return this.subscriptionService.getSubscriptionsPerMonth(
+      year,
+      status,
+      date_filter,
+    );
+  }
+
+  @Get('/charges-per-month')
+  async getSubscriptionChargesPerMonth(
+    @Query('year')
+    year?: number,
+    @Query('status')
+    status?: number,
+    @Query('date_filter')
+    date_filter?: number,
+  ): Promise<{ month: number; count: number }[]> {
+    return this.subscriptionService.getSubscriptionChargesPerMonth(
+      year,
+      status,
+      date_filter,
+    );
+  }
+
+  @Get('/charge-period-per-month')
+  async getSubscriptionChargePeriodPerMonth(
+    @Query('year')
+    year?: number,
+    @Query('status')
+    status?: number,
+    @Query('date_filter')
+    date_filter?: number,
+  ): Promise<{ month: number; count: number }[]> {
+    return this.subscriptionService.getSubscriptionChargePeriodPerMonth(
+      year,
+      status,
+      date_filter,
+    );
+  }
+
+  @Get('/value-per-month')
+  async getSubscriptionValuePeriodPerMonth(
+    @Query('year')
+    year?: number,
+    @Query('status')
+    status?: number,
+    @Query('date_filter')
+    date_filter?: number,
+  ): Promise<{ month: number; count: number }[]> {
+    return this.subscriptionService.getSubscriptionValuePeriodPerMonth(
+      year,
+      status,
+      date_filter,
+    );
   }
 }
--- backend/src/subscriptions/subscription.service.ts
+++ backend/src/subscriptions/subscription.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { InjectRepository, InjectEntityManager } from '@nestjs/typeorm';
+import { Repository, EntityManager } from 'typeorm';
 import { Subscription } from './subscription.entity';
 
 @Injectable()
@@ -8,9 +8,192 @@ export class SubscriptionService {
   constructor(
     @InjectRepository(Subscription)
     private subscriptionRepository: Repository<Subscription>,
+    @InjectEntityManager()
+    private entityManager: EntityManager,
   ) {}
 
   async findAll(): Promise<Subscription[]> {
     return await this.subscriptionRepository.find();
+  }
+
+  // Incrições por mês
+  async getSubscriptionsPerMonth(
+    year?: number,
+    status?: number,
+    date_filter?: number,
+  ): Promise<{ month: number; count: number }[]> {
+    let whereClause = '';
+
+    // Filtro de ano
+    if (year !== undefined) {
+      whereClause = `WHERE YEAR(start_date) = ${year}`;
+    }
+
+    // Filtro de status
+    if (status !== undefined) {
+      if (whereClause != '') {
+        whereClause = whereClause + ' and ';
+      } else {
+        whereClause = 'WHERE ';
+      }
+      whereClause = whereClause + `status = ${status}`;
+    }
+
+    // Filtro de tipo de data
+    let dateFilter = 'start_date';
+    if (date_filter != undefined) {
+      if (date_filter == 2) {
+        dateFilter = 'status_date';
+      } else if (date_filter == 3) {
+        dateFilter = 'cancel_date';
+      } else {
+        dateFilter = 'next_cycle';
+      }
+    }
+
+    const query = `
+        SELECT MONTH(${dateFilter}) as month, COUNT(*) as count
+        FROM subscriptions
+        ${whereClause}
+        GROUP BY MONTH(${dateFilter})
+    `;
+
+    const result = await this.entityManager.query(query);
+    return result;
+  }
+
+  // Cobranças por mês
+  async getSubscriptionChargesPerMonth(
+    year?: number,
+    status?: number,
+    date_filter?: number,
+  ): Promise<{ month: number; count: number }[]> {
+    let whereClause = '';
+
+    // Filtro de ano
+    if (year !== undefined) {
+      whereClause = `WHERE YEAR(start_date) = ${year}`;
+    }
+
+    // Filtro de status
+    if (status !== undefined) {
+      if (whereClause != '') {
+        whereClause = whereClause + ' and ';
+      } else {
+        whereClause = 'WHERE ';
+      }
+      whereClause = whereClause + `status = ${status}`;
+    }
+
+    // Filtro de tipo de data
+    let dateFilter = 'start_date';
+    if (date_filter != undefined) {
+      if (date_filter == 2) {
+        dateFilter = 'status_date';
+      } else if (date_filter == 3) {
+        dateFilter = 'cancel_date';
+      } else {
+        dateFilter = 'next_cycle';
+      }
+    }
+
+    const query = `
+      SELECT MONTH(${dateFilter}) as month, SUM(quantity_of_charges) as count
+      FROM subscriptions
+      ${whereClause}
+      GROUP BY MONTH(${dateFilter})
+    `;
+    const result = await this.entityManager.query(query);
+    return result;
+  }
+
+  // Período de cobrança
+  async getSubscriptionChargePeriodPerMonth(
+    year?: number,
+    status?: number,
+    date_filter?: number,
+  ): Promise<{ month: number; count: number }[]> {
+    let whereClause = '';
+
+    // Filtro de ano
+    if (year !== undefined) {
+      whereClause = `WHERE YEAR(start_date) = ${year}`;
+    }
+
+    // Filtro de status
+    if (status !== undefined) {
+      if (whereClause != '') {
+        whereClause = whereClause + ' and ';
+      } else {
+        whereClause = 'WHERE ';
+      }
+      whereClause = whereClause + `status = ${status}`;
+    }
+
+    // Filtro de tipo de data
+    let dateFilter = 'start_date';
+    if (date_filter != undefined) {
+      if (date_filter == 2) {
+        dateFilter = 'status_date';
+      } else if (date_filter == 3) {
+        dateFilter = 'cancel_date';
+      } else {
+        dateFilter = 'next_cycle';
+      }
+    }
+
+    const query = `
+      SELECT MONTH(${dateFilter}) as month, SUM(charge_period) as count
+      FROM subscriptions
+      ${whereClause}
+      GROUP BY MONTH(${dateFilter})
+    `;
+    const result = await this.entityManager.query(query);
+    return result;
+  }
+
+  // Valor por mês
+  async getSubscriptionValuePeriodPerMonth(
+    year?: number,
+    status?: number,
+    date_filter?: number,
+  ): Promise<{ month: number; count: number }[]> {
+    let whereClause = '';
+
+    // Filtro de ano
+    if (year !== undefined) {
+      whereClause = `WHERE YEAR(start_date) = ${year}`;
+    }
+
+    // Filtro de status
+    if (status !== undefined) {
+      if (whereClause != '') {
+        whereClause = whereClause + ' and ';
+      } else {
+        whereClause = 'WHERE ';
+      }
+      whereClause = whereClause + `status = ${status}`;
+    }
+
+    // Filtro de tipo de data
+    let dateFilter = 'start_date';
+    if (date_filter != undefined) {
+      if (date_filter == 2) {
+        dateFilter = 'status_date';
+      } else if (date_filter == 3) {
+        dateFilter = 'cancel_date';
+      } else {
+        dateFilter = 'next_cycle';
+      }
+    }
+
+    const query = `
+      SELECT MONTH(${dateFilter}) as month, SUM(value) as count
+      FROM subscriptions
+      ${whereClause}
+      GROUP BY MONTH(${dateFilter})
+    `;
+    const result = await this.entityManager.query(query);
+    return result;
   }
 }
